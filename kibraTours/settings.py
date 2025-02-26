9b446af2cee851fa0062ad57a2698f6a747f0fe3
Christinekimani <christinekimani881@gmail.com>
2025-02-26 16:54:58 +0300

    Force SQLite database

--- kibraTours/settings.py
+++ kibraTours/settings.py
@@ -83,10 +83,14 @@ WSGI_APPLICATION = 'kibraTours.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
-}
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
